83cd87f0b5ce9cae4dd222090348254d4f941a26
southorange0929 <southorange0929@foxmail.com>
2024-05-15 20:21:13 +0800

    feat: add use-native example

---
+++ use-native/entry/src/main/cpp/types/libentry/index.d.ts
@@ -0,0 +1,4 @@
+import buffer from '@ohos.buffer';
+
+export function crc32(input: string | buffer.Buffer, crc?: number): number
+export function crc32c(input: string | buffer.Buffer, crc?: number): number
