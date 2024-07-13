8f65e15f8106ef4c28d9f52dd0df0236e4c1e291
southorange0929 <southorange0929@foxmail.com>
2024-07-13 11:14:17 +0800

    feat: zig

---
+++ zig-example/zig-lib/build.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const libfizzbuzz = b.addSharedLibrary(.{
+        .name = "fizzbuzz",
+        .root_source_file = b.path("fizzbuzz.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "hello",
+        .root_source_file = b.path("hello.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(exe);
+
+    b.installArtifact(libfizzbuzz);
+}
---
+++ zig-example/zig-lib/fizzbuzz.zig
@@ -0,0 +1,12 @@
+export fn fizzbuzz(n: usize) ?[*:0]const u8 {
+    if (n % 5 == 0) {
+        if (n % 3 == 0) {
+            return "fizzbuzz";
+        } else {
+            return "fizz";
+        }
+    } else if (n % 3 == 0) {
+        return "buzz";
+    }
+    return null;
+}
